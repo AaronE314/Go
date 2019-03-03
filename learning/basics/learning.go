b625b5f9631b9f30dd7e08b138b208c5e1ba8a40
Aaron <aaronde314@gmail.com>
2019-03-03 16:12:53 -0500

    init

---
+++ learning/basics/learning.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+import "math"
+
+// Gobyexample.com
+
+const s string = "constant"
+
+func main() {
+
+	// Hello World
+	fmt.Println("hello world")
+
+	// Values
+	fmt.Println("go" + "lang")
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	// Variables
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+
+	// Constants
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const m = 3e20 / n
+	fmt.Println(d)
+
+	fmt.Println(int64(m))
+
+	fmt.Println(math.Sin(n))
+
+}
